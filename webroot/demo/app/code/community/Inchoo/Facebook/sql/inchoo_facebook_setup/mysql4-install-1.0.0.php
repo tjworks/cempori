ce66fdeb5bb3529cfa8e915a49a3c28e56ab4e38
havoc <1001code@gmail.com>
2013-02-18 00:42:44 +0800

    修改用户注册属性
    update 1.0.0

--- webroot/demo/app/code/community/Inchoo/Facebook/sql/inchoo_facebook_setup/mysql4-install-1.0.0.php
+++ webroot/demo/app/code/community/Inchoo/Facebook/sql/inchoo_facebook_setup/mysql4-install-1.0.0.php
@@ -6,6 +6,7 @@
  * @copyright   Inchoo (http://inchoo.net)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
+
 $installer = $this;
 /* @var $installer Mage_Customer_Model_Entity_Setup */
 
