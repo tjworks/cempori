6d33e64d9e2ae1ee1f52f6fe217cb0e3b8071d6f
TJ <evertang@gmail.com>
2013-02-21 16:03:21 +0000

    CAddThis and other plugin

---
+++ webroot/demo/app/code/community/BeeShopy/BeeSocial/Model/Themes.php
@@ -0,0 +1,12 @@
+<?php
+class BeeShopy_BeeSocial_Model_Themes
+{
+    public function toOptionArray()
+    {
+        return array(
+          array('value'=>"light", 'label'=> "Light"),
+          array('value'=> "dark", 'label'=> "Dark")
+        );
+    }
+}
+
