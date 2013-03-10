7ca4a424b66b67029f9f6c30200d425ad68520eb
TJ <evertang@gmail.com>
2013-03-10 10:36:28 -0500

    Experiment extension initial check in

---
+++ webroot/demo/app/code/local/Tjworks/Experiment/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+class Tjworks_Experiment_IndexController extends Mage_Core_Controller_Front_Action {        
+
+    public function indexAction() {
+
+        echo 'Hello Index! GG';
+
+    }
+    public function storefrontAction(){
+      
+        //echo "Store front!";
+    }
+  
+}
+?>
+
+
---
+++ webroot/demo/app/code/local/Tjworks/Experiment/controllers/SplashController.php
@@ -0,0 +1,10 @@
+<?php
+class Tjworks_Experiment_SplashController extends Mage_Core_Controller_Front_Action {        
+    public function indexAction() {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
+?>
+
+
